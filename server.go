40d81ba6c011570c2af67f8a6e33ed6a4e74744b
Devon <k849996781@vip.qq.com>
2018-01-23 10:06:56 +0800

    test

--- server.go
+++ server.go
@@ -1,8 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Edu struct
+{
+	School string
+	Professional string
+	Level string
+	Dec string
+	Time string
+}
+
+type User struct
+{
+	Name string
+	Edu_list []Edu
+}
 
 func main() {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("test").C("people")
+	var users []User 
+	c.Find(&bson.M{"_id": bson.ObjectIdHex("5a657f7c0dd364221c83784b")}).All(&users) 
+	fmt.Println(users)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
